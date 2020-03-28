1739900d264519c5d0305d645a5427cd12531be5
oxygene4 <5797565@gmail.com>
2020-03-28 13:25:15 +0200

    initial

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const handler = require(path.join(__dirname, 'src'))
+const app = express()
 
-express().get('/', (req, res) => res.json(handler())).listen(PORT)
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
+app.get('/', (req, res) => res.json(handler())).listen(PORT)
