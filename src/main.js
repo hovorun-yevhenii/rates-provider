e1f8aac9e0bd51cbcdf37875c2a553c7c64b975d
oxygene4 <5797565@gmail.com>
2020-03-31 09:30:32 +0300

    replace readFile with require

--- src/main.js
+++ src/main.js
@@ -1,20 +1,18 @@
 const fs = require('fs');
 const path = require('path')
 const {minutesPassed, fetchRates} = require(path.resolve(__dirname, 'utils.js'))
+const ratesPath = path.resolve(__dirname, 'rates.json')
 const CACHE_TIME_IN_MINUTES = 60
 
 module.exports = async () => {
-    const ratesPath = path.resolve(__dirname, 'rates.json')
-    const rawRates = fs.readFileSync(ratesPath)
-    const cachedRates = JSON.parse(rawRates.toString())
-
+    const cachedRates = require(ratesPath);
     const timeDiff = minutesPassed(cachedRates.timestamp)
     const shouldUpdate = timeDiff > CACHE_TIME_IN_MINUTES
 
     if (shouldUpdate) {
         const newRates = await fetchRates()
 
-        fs.writeFileSync(ratesPath, JSON.stringify(newRates))
+        fs.writeFile(ratesPath, JSON.stringify(newRates), () => {});
 
         return newRates
     } else {
