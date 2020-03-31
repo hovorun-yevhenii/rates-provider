d3f42e85cba84698161262ef0ecef967515fc0f7
oxygene4 <5797565@gmail.com>
2020-03-31 12:16:37 +0300

    replace https with request

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,8 @@
-const https = require('https');
+const request = require('request')
 const KEY_PRIME = '4094e5dbb7d1407ea5ce2820c7a781ad'
 const KEY_RESERVE = '027700360ab349d0b70f7d7098a8883d'
 
-function minutesPassed(date){
+function minutesPassed(date) {
     const diff = (+new Date() - date) / 1000 / 60
     return Math.abs(Math.round(diff))
 }
@@ -11,19 +11,19 @@ function fetchRates(prime) {
     return new Promise(resolve => {
         const BASE_URL = 'https://openexchangerates.org/api/'
         const KEY = prime ? KEY_PRIME : KEY_RESERVE
+        const url = `${BASE_URL}latest.json?app_id=${KEY}`
 
-        https.get(`${BASE_URL}latest.json?app_id=${KEY}`, (res) => {
-            res.on('data', data => {
-                const response = JSON.parse(data.toString())
-                const {base, rates} = response
-
-                resolve({base, rates, timestamp: +new Date()})
-            })
-        }).on('error', () => {
-            if (prime) {
+        request.get({url, json: true}, (err, res, data) => {
+            if (err || res.statusCode !== 200) {
                 return fetchRates()
+            } else {
+                resolve({
+                    base: data.base,
+                    rates: data.rates,
+                    timestamp: +new Date()
+                })
             }
-        });
+        })
     })
 }
 
